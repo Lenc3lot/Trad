fa6c93da125e33339bf9d8491a4c22ddaa0ee1db
Lenc3lot <noto.baptiste2910@gmail.com>
2023-10-02 19:47:13 +0200

    Update tester.php

--- tonton/tester.php
+++ tonton/tester.php
@@ -4,38 +4,65 @@ use HJSON\HJSONParser;
 
 $option = ['keepWsc' => false, 'assoc' => true];
 
-
 $data = file_get_contents("./base1311/Mods.CalamityMod.Attunement.hjson");
 $parser = new HJSONParser();
 $obj = $parser->parse($data, $option);
 
-$dataFR = file_get_contents("./base1311/Mods.CalamityMod.Configs.hjson");
+$dataFR = file_get_contents("./base1311/Mods.CalamityMod.Attunement.hjson");
 $objFR = $parser->parse($dataFR, $option);
 json_encode($objFR);
 //var_dump($objFR);
+// $retourTab = array();
+// $dataTotal = array();
 
+?> 
+
+<table>
+
+	<tr>
+		<th> EN_Id </th>
+		<th> EN_name </th>
+		<th> EN_desc </th>
+		<th> FR_Id </th>
+		<th> FR_name </th>
+		<th> FR_desc </th>
+	<tr>
+
+
+<?php
 foreach ($objFR as $key => $element) {
-	echo "<b style='font-size:1.75em'>" . $key . "</b>" . " :<br>	";
+	echo "
+	<tr> 		
+		<td>".$key."</td><td>";
 	if (gettype($element) != 'array') {
-		echo $element;
+		//echo "element1 = ".$element;
 	} else {
+
 		foreach ($element as $key2 => $souselement) {
-			echo "<b>" . $key2 . " :</b><br/> ";
+			//echo " key2 = " . htmlspecialchars($key2) . " : ";
+			echo htmlspecialchars($key2) ." : ".htmlspecialchars($souselement)." <br>";
+			
 			if (gettype($key2) != 'array') {
+
 				if (gettype($souselement) == 'array') {
+
 					foreach ($souselement as $key3 => $text) {
-						echo " \_>	<b>".$key3." :</b> ".$text.'<br/>';
+						echo " key3 = ".htmlspecialchars($key3)." : ".$text.'';
 					}
-				} else {
-					echo $souselement.'<br/>';
-				}
+
+				}// else {
+
+				// 	//echo "element2 =".htmlspecialchars($souselement);
+				
+				// }
 			}
 		}
+		echo "</td>";
+		
 	}
-	echo "<br><br>";
-
+	
 }
+echo "</tr>";
+echo "</table>";
 
-
-//print_r($tab);
 ?>
