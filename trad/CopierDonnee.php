b044c6804d91862cb83117c9ee8d703416bfc6ee
Lenc3lot <noto.baptiste2910@gmail.com>
2023-10-04 16:21:13 +0200

    submit

--- trad/CopierDonnee.php
+++ trad/CopierDonnee.php
@@ -3,16 +3,14 @@
     use HJSON\HJSONParser;
 
     $option = ['keepWsc' => false, 'assoc' => true];
-    $data = file_get_contents("./base1311/Mods.CalamityMod.Attunement.hjson");
+
+    $data = file_get_contents("./outputFR/fr-FR_Mods.CalamityMod.Attunement.hjson");
     $parser = new HJSONParser();
     $obj = $parser->parse($data, $option);
 
-
     //print_r($_POST);
-    // $monAttribut = $_POST["monAttribut"];
-    // $contenu = $_POST["contenu"];
-    
-    
+    $monAttribut = $_POST["monAttribut"];
+    $contenu = $_POST["contenu"];
 
     // echo $monAttribut[0];
     // $ligne = $monAttribut[1]." ".$contenu."\n";
@@ -21,10 +19,10 @@
     // $data .= $ligne;
     // $data = file_put_contents($file,$data);
 
-    $monAttribut = "DefaultAttunement.Name";
-    $contenu = "Bonjour Test 2 !";
+    // $monAttribut = "DefaultAttunement.Name";
+    // $contenu = "Bonjour Test 2 !";
 
-    $jsonmodifiable = "./test.json";
+    // $jsonmodifiable = "./Copytest.json";
     $tabModif = explode(".",$monAttribut);
     $monTabCourant = $obj;
     $monAdr = &$monTabCourant;
