206c3336449e4d17a7f2381eb5d27600abde1ad0
Lenc3lot <noto.baptiste2910@gmail.com>
2023-10-06 14:04:42 +0200

    V1.0
    Version utilisable,
    Remplace le contenu du fichier HJSON par la modification effectuée.
    Le fichier est ensuite téléchargeable directement grâce au bouton.

--- trad/script.js
+++ trad/script.js
@@ -1,12 +1,10 @@
 function MaFonction(monOption){
     let monPath = monOption.getAttribute('data-path');
-    alert("bonjour " +monPath);
 }
 
 function sendData(monInput){
     let monAttribut = monInput.getAttribute('data-id');
     let contenu = monInput.value;
-    alert (monAttribut + " " +contenu);
     $.ajax({
         url:"http://localhost/Trad/trad/CopierDonnee.php",
         type:"post",
@@ -21,5 +19,4 @@ function sendData(monInput){
         }
 
     })
-    // alert(monInput.getAttribute('data-id') + ' ' + monInput.parentNode.parentNode.getElementsByTagName('TD')[1].innerHTML); //avant l'espace vide = le data-id à la chaine "tab1.tab2..." de l'autre côté
 } 
